2388a0b3c1dcbeb487c338c046b2c2a2bc3c567a
OxY623 <fuzzydoorproductions0@gmail.com>
2024-07-20 16:06:52 +0300

    Готова верстка и первая часть работы

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -17,39 +17,25 @@ const MovieCards = ({ cards, getGenres }) => {
         gap: 36,
       }}
     >
-      {cards.map((card) => (
-        <li key={card.id} className="card-item">
+      {cards.map((card, index) => (
+        <li key={`${card.title}-${index}`} className="card-item">
           <Card className="card-item__card" style={{ display: 'flex' }}>
-            <div className="card-item__content" style={{ display: 'flex', flexDirection: 'row' }}>
+            <div className="card-item__content">
               <img
                 className="card-item__image"
                 alt={card.title}
                 src={card.poster_path ? `https://image.tmdb.org/t/p/w500${card.poster_path}` : noImage}
                 style={{ objectFit: 'cover' }}
               />
-              <div className="card-item__details" style={{ marginLeft: 20, flex: 1 }}>
-                <div className="card-item__header" style={{ textAlign: 'left', display: 'flex', flexDirection: 'row' }}>
+              <div className="card-item__details">
+                <div className="card-item__header">
                   <h3 className="card-item__title">{card.title}</h3>
-                  <div
-                    className="card-item__rating"
-                    style={{
-                      border: 'border: 2px solid rgb(233, 209, 0)',
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      color: 'rgb(0, 0, 0)',
-                      borderRadius: '50%',
-                      width: 30,
-                      height: 30,
-                    }}
-                  >
-                    {card.vote_average.toFixed(1)}
-                  </div>
+                  <div className="card-item__rating">{card.vote_average.toFixed(1)}</div>
                 </div>
                 <p className="card-item__release-date">{format(new Date(card.release_date), 'MMMM d, yyyy')}</p>
                 <div className="card-item__genres">
-                  {getGenres(card.genre_ids).map((genre) => (
-                    <Tag key={genre}>{genre}</Tag>
+                  {getGenres(card.genre_ids).map((genre, index) => (
+                    <Tag key={index}>{genre}</Tag>
                   ))}
                 </div>
                 <p className="card-item__overview">{card.overview}</p>
