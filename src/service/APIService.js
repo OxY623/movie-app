2b78f9787240b87fc1f0380ae3ff3597ec869c0c
OxY623 <fuzzydoorproductions0@gmail.com>
2024-07-31 20:35:39 +0300

    Исправление и доработки

--- src/service/APIService.js
+++ src/service/APIService.js
@@ -1,23 +1,13 @@
+import './http'
+
 export default class SearchService {
   constructor(baseURL, apiKey) {
     this.baseURL = baseURL
     this.apiKey = apiKey
   }
-
+  //
+  //Получение списка фильмов по запросу из поиска
   async getResults(query = 'search', page = 1) {
-    // if (!query) {
-    //   throw new Error('❌ Query is required')
-    // }
-    // if (query.length === 0) {
-    //   throw new Error('❌ Пустой поисковый запрос')
-    // }
-    // if (page <= 0) {
-    //   throw new Error('❌ Номер страницы должен быть положительным числом')
-    // }
-    // if (query.length === 0 && page > 1) {
-    //   throw new Error('❌ Такого не может быть')
-    // }
-
     const response = await fetch(
       // `${this.baseURL}/3/search/movie?api_key=${this.apiKey}&query=${encodeURIComponent(query)}`
       `${this.baseURL}/3/search/movie?api_key=${this.apiKey}&query=${encodeURIComponent(query || '  ')}&page=${page}`,
